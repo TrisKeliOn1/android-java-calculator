3502e4224b0f2bdc6e495c9510c6846f4757a665
Hebert <hebertLandim007@outlook.com>
2023-01-24 19:45:31 -0300

    Selecting Buttons

--- src/main/java/com/mobile/calculator/MainActivity.java
+++ src/main/java/com/mobile/calculator/MainActivity.java
@@ -3,8 +3,17 @@ package com.mobile.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button numeroZero, numeroUm, numeroDois, numeroTres, numeroQuatro, numeroCinco, numeroSeis, numeroSete, numeroOito,
+    numeroNove, ponto, soma, subtracao, multiplicacao, divisao, igual, botao_limpar;
+
+    private TextView txtExpressao, txtResultado;
+    private ImageView backspace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
