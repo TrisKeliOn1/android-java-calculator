bfc5c14515355c457c0be2db324cd50fe37fd3a7
Hebert <hebertLandim007@outlook.com>
2023-01-30 10:18:44 -0300

    finished

--- src/main/java/com/mobile/calculator/MainActivity.java
+++ src/main/java/com/mobile/calculator/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button numeroZero, numeroUm, numeroDois, numeroTres, numeroQuatro, numeroCinco, numeroSeis, numeroSete, numeroOito,
@@ -52,10 +55,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         backspace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 TextView expressao = findViewById(R.id.txt_expressao);
                 String string = expressao.getText().toString();
 
                 if (!string.isEmpty()) {
+
                     byte var0 = 0;
                     int var1 = string.length()-1;
                     String txtExpressao = string.substring(var0, var1);
@@ -64,6 +69,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 txtResultado.setText("");
             }
         });
+
+        igual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Expression expressao = new ExpressionBuilder(txtExpressao.getText().toString()).build();
+                    double resultado = expressao.evaluate();
+                    long longResult = (long) resultado;
+
+                    if (resultado == (double) longResult) {
+                        txtResultado.setText((CharSequence) String.valueOf(longResult));
+                    } else {
+                        txtResultado.setText((CharSequence) String.valueOf(resultado));
+                    }
+                } catch (Exception e) {
+
+                }
+
+            }
+        });
+
     }
 
     private void IniciarComponentes(){
@@ -92,16 +119,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void AcrescentarUmaExpressao(String string, boolean limpar_dados){
 
         if (txtResultado.getText().equals("")){
-            txtExpressao.setText("");
+            txtExpressao.setText(" ");
         }
 
         if (limpar_dados){
-            txtResultado.setText("");
+            txtResultado.setText(" ");
             txtExpressao.append(string);
         }else{
             txtExpressao.append(txtResultado.getText());
             txtExpressao.append(string);
-            txtResultado.setText("");
+            txtResultado.setText(" ");
         }
     }
 
